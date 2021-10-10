ef7b4453546d9930490feccfb07a48e780ee34e9
Manik Kumar Shil <68231832+Manikshil@users.noreply.github.com>
2021-10-10 22:21:34 +0600

    Add files via upload

---
+++ map/maplearning.js
@@ -0,0 +1,34 @@
+// map method 
+
+const people =[
+
+    {
+        name: 'bob',
+        age: 20,
+        position: 'developer',
+      },
+      {
+        name: 'anna',
+        age: 25,
+        position: 'designer',
+      },
+      {
+        name: 'susy',
+        age: 30,
+        position: 'the boss',
+      },
+      {
+        name: 'john',
+        age: 26,
+        position: 'intern',
+      },
+
+];
+
+// returns a new array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one
+
+const ages = people.map(()=>{})
+console.log(ages);
+
